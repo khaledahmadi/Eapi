55ce95862d4d355118eef9dcd3a4c7f379d54126
khaledahmadi <khaledahmadi556@gmail.com>
2017-11-13 18:26:56 +0800

    Created Model -a

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/Products', 'ProductsController');
+Route::group(['prefix'=>'porducts'], function(){
+    Route::apiResource('{products}/reviews', 'ReviewsController');
+});
